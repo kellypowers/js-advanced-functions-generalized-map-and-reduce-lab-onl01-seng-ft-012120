c9c0c4f7f6b32ed27572bd519b95e5d02f530947
kellypowers <kpowers.nj@gmail.com>
2020-04-27 17:15:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+function map(arr, fn) {
+    let newArr = []
+  
+    for (let i = 0; i < arr.length; i++) {
+      let theElement = arr[i]
+      newArr.push(fn(theElement))
+    }
+  
+    return newArr;
+  }
+  
+  function reduce(arr, fn, starting){
+    let newArr = (!!starting) ? starting : arr[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < arr.length; i++) {
+      newArr = fn(arr[i], newArr)
+    }
+  
+    return newArr;
+  }
